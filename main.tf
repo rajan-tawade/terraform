c03eae0378294ae356f2b0c77e393f84f1e0299b
Rajan Tawade <rajan.tawade@gmail.com>
2024-11-01 22:59:03 +0000

    update

--- main.tf
+++ main.tf
@@ -1,18 +1,51 @@
+
+variable "project" {
+  type = string
+  default = "devops-01-354720"
+}
+
+variable "region" {
+  type = string
+  default = "europe-west2"
+}
+
+variable "zone" {
+  type = string
+  default = "europe-west2-a"
+}
+
+variable "name" {
+  type = string
+  default = "terraform-instance"
+}
+
+variable "machine_type" {
+  type = string
+  default = "e2-micro"
+}
+
+variable "image" {
+  type = string
+  default = "debian-cloud/debian-11"
+}
+
+
+
 provider "google" {
   credentials = file("google.json")
-  project = "devops-01-354720"
-  region = "europe-west2"
-  zone = "europe-west2-a"
+  project = var.project
+  region = var.region
+  zone = var.zone
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name = "terraform-instance"
-  machine_type = "e2-micro"
-  zone = "europe-west2-a"
+  name = var.name
+  machine_type = var.machine_type
+  zone = var.zone
   
   boot_disk {
     initialize_params {
-		image = "debian-cloud/debian-11"
+		image = var.image
 	}
   }
 
