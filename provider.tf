5e307a61276a85b52c813c7a03fff3f7757433f6
Rajan Tawade <rajan.tawade@gmail.com>
2024-11-02 17:24:23 +0000

    Update

--- provider.tf
+++ provider.tf
@@ -1,7 +1,15 @@
 provider "google" {
-  credentials = ${file("google.json")}
-  project = "${var.project}"
-  region = "${var.region}"
-  zone = "${var.zone}"
-  
+  credentials = file("google.json")
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+
 }
+
+terraform {
+  backend "gcs" {
+    bucket  = "devops-sample-bucket-my"
+    prefix  = "terraform/state"
+  }
+}
+
